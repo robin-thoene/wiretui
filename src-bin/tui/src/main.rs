b52012d0bd3c56fa9c28518c11fa44ab265a7060
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2025-11-12 22:22:40 +0100

    feat: setup logger on app startup

--- src-bin/tui/src/main.rs
+++ src-bin/tui/src/main.rs
@@ -4,19 +4,29 @@ use application::{
     deactivate_connection_usecase::DeactivateConnectionUsecase,
     list_connections_usecase::ListConnectionsUseCase,
 };
+use env_logger::Env;
 use std::io;
 
 fn main() -> io::Result<()> {
+    // Setup the logger
+    let env = Env::default().filter_or("RUST_LOG", "info");
+    env_logger::init_from_env(env);
+    log::debug!("Configured logger");
     // Build the dependencies
     let wireguard_dbus_adapter = WireGuardDBusRepository::new().expect("TODO: fix later");
     let list_connections_usecase = ListConnectionsUseCase::new(&wireguard_dbus_adapter);
     let activate_connection_usecase = ActivateConnectionUsecase::new(&wireguard_dbus_adapter);
     let deactivate_connection_usecase = DeactivateConnectionUsecase::new(&wireguard_dbus_adapter);
+    log::debug!("Buit the dependencies");
     // Build and run the TUI application
     let mut tui_app = App::new(
         list_connections_usecase,
         activate_connection_usecase,
         deactivate_connection_usecase,
     );
-    tui_app.run()
+    log::debug!("Created the TUI application");
+    log::info!("Starting TUI application ...");
+    let result = tui_app.run();
+    log::info!("TUI application stopped");
+    result
 }
