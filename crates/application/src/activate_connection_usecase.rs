ecefe2ebc10964dfa6cd14abe9585e12bca14d6a
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2026-04-16 22:21:18 +0200

    feat(test): add success test cases for conn activation use case

--- crates/application/src/activate_connection_usecase.rs
+++ crates/application/src/activate_connection_usecase.rs
@@ -85,3 +85,235 @@ where
         }
     }
 }
+
+#[cfg(test)]
+mod activate_connection_usecase_tests {
+    use super::*;
+    use ports::outbound::wireguard_port::{
+        ConnectionActivationError, ConnectionDeactivationError, GetConnectionsError,
+    };
+    use std::cell::RefCell;
+
+    struct WireGuardDbusRepoMock {
+        available_connections: RefCell<Vec<WireGuardConnection>>,
+    }
+
+    impl WireGuardDbusRepoMock {
+        /// Initialize the mock implementation with a single non-active connection
+        pub fn init_single_conn() -> Self {
+            Self {
+                available_connections: RefCell::new(vec![WireGuardConnection::new(
+                    "some-id".into(),
+                    false,
+                )]),
+            }
+        }
+
+        /// Initialize the mock implementation with a bunch of non-active connections
+        pub fn init_conn_list() -> Self {
+            Self {
+                available_connections: RefCell::new(vec![
+                    WireGuardConnection::new("some-id-1".into(), false),
+                    WireGuardConnection::new("some-id-2".into(), false),
+                    WireGuardConnection::new("some-id-3".into(), false),
+                    WireGuardConnection::new("some-id-4".into(), false),
+                ]),
+            }
+        }
+
+        /// Initialize the mock implementation with a bunch of non-active connections
+        /// and one that is currently marked as active
+        pub fn init_conn_list_with_active() -> Self {
+            Self {
+                available_connections: RefCell::new(vec![
+                    WireGuardConnection::new("some-id-1".into(), false),
+                    WireGuardConnection::new("some-id-2".into(), false),
+                    WireGuardConnection::new("some-id-3".into(), false),
+                    WireGuardConnection::new("some-id-4".into(), true),
+                ]),
+            }
+        }
+
+        /// Initialize the mock implementation with a bunch of non-active connections
+        /// and multiple that are currently marked as active
+        pub fn init_conn_list_with_multi_active() -> Self {
+            Self {
+                available_connections: RefCell::new(vec![
+                    WireGuardConnection::new("some-id-1".into(), false),
+                    WireGuardConnection::new("some-id-2".into(), false),
+                    WireGuardConnection::new("some-id-3".into(), true),
+                    WireGuardConnection::new("some-id-4".into(), true),
+                ]),
+            }
+        }
+
+        /// Get the internal mock state of all connections
+        pub fn get_all_connections(&self) -> Vec<WireGuardConnection> {
+            let conn: Vec<WireGuardConnection> = self
+                .available_connections
+                .borrow()
+                .iter()
+                .map(|x| WireGuardConnection::new(x.get_id().into(), *x.get_is_active()))
+                .collect();
+            conn
+        }
+
+        /// Get the internal mock state of connections that are marked as active
+        pub fn get_active_connections(&self) -> Vec<WireGuardConnection> {
+            let conn: Vec<WireGuardConnection> = self
+                .available_connections
+                .borrow()
+                .iter()
+                .filter(|x| x.get_is_active() == &true)
+                .map(|x| WireGuardConnection::new(x.get_id().into(), *x.get_is_active()))
+                .collect();
+            conn
+        }
+
+        /// Internally mark the connection as activated/deactivated if it exists in the mock state
+        pub fn set_internal_conn_state(&self, id: &str, is_active: bool) {
+            let mut mutb = self.available_connections.borrow_mut();
+            let idx = mutb.iter().position(|x| x.get_id() == id);
+            if let Some(idx) = idx {
+                mutb[idx] = WireGuardConnection::new(id.into(), is_active);
+            }
+        }
+    }
+    impl WireGuardPort for WireGuardDbusRepoMock {
+        fn get_imported_connections(
+            &self,
+        ) -> Result<Vec<WireGuardConnection>, GetConnectionsError> {
+            let conns: Vec<WireGuardConnection> = self.get_all_connections();
+            Ok(conns)
+        }
+
+        fn activate_connection(&self, id: &str) -> Result<(), ConnectionActivationError> {
+            self.set_internal_conn_state(id, true);
+            Ok(())
+        }
+
+        fn deactivate_connection(&self, id: &str) -> Result<(), ConnectionDeactivationError> {
+            self.set_internal_conn_state(id, false);
+            Ok(())
+        }
+    }
+
+    /// Ensures that activating a single imported connection that not yet is active works
+    #[test]
+    fn success_single_available_connection() {
+        // Arrange
+        let dbus_repo_mock = WireGuardDbusRepoMock::init_single_conn();
+        let id_to_activate = "some-id";
+        // Act
+        let result = ActivateConnectionUsecase::new(&dbus_repo_mock)
+            .activate(&WireGuardConnection::new(id_to_activate.into(), false));
+        // Assert
+        assert!(result.is_ok());
+        assert_eq!(dbus_repo_mock.get_active_connections().iter().len(), 1);
+        assert!(
+            dbus_repo_mock
+                .get_active_connections()
+                .first()
+                .expect("a single active connection exists here")
+                .get_is_active(),
+        );
+        assert_eq!(
+            dbus_repo_mock
+                .get_active_connections()
+                .first()
+                .expect("a single active connection exists here")
+                .get_id(),
+            id_to_activate
+        );
+    }
+
+    /// Ensures that activating a single imported connection that not yet is active works for
+    /// an available list of not yet activated connections
+    #[test]
+    fn success_multiple_available_connection() {
+        // Arrange
+        let dbus_repo_mock = WireGuardDbusRepoMock::init_conn_list();
+        let id_to_activate = "some-id-4";
+        // Act
+        let result = ActivateConnectionUsecase::new(&dbus_repo_mock)
+            .activate(&WireGuardConnection::new(id_to_activate.into(), false));
+        // Assert
+        assert!(result.is_ok());
+        assert_eq!(dbus_repo_mock.get_active_connections().iter().len(), 1);
+        assert!(
+            dbus_repo_mock
+                .get_active_connections()
+                .first()
+                .expect("a single active connection exists here")
+                .get_is_active(),
+        );
+        assert_eq!(
+            dbus_repo_mock
+                .get_active_connections()
+                .first()
+                .expect("a single active connection exists here")
+                .get_id(),
+            id_to_activate
+        );
+    }
+
+    /// Ensures that activating one connection automatically deactivates another that is currently
+    /// active
+    #[test]
+    fn success_currently_active_connection() {
+        // Arrange
+        let dbus_repo_mock = WireGuardDbusRepoMock::init_conn_list_with_active();
+        let id_to_activate = "some-id-2";
+        // Act
+        let result = ActivateConnectionUsecase::new(&dbus_repo_mock)
+            .activate(&WireGuardConnection::new(id_to_activate.into(), false));
+        // Assert
+        assert!(result.is_ok());
+        assert_eq!(dbus_repo_mock.get_active_connections().iter().len(), 1);
+        assert!(
+            dbus_repo_mock
+                .get_active_connections()
+                .first()
+                .expect("a single active connection exists here")
+                .get_is_active(),
+        );
+        assert_eq!(
+            dbus_repo_mock
+                .get_active_connections()
+                .first()
+                .expect("a single active connection exists here")
+                .get_id(),
+            id_to_activate
+        );
+    }
+
+    /// Ensures that activating one connection automatically deactivates all others that are
+    /// currently active
+    #[test]
+    fn success_currently_multiple_active_connections() {
+        // Arrange
+        let dbus_repo_mock = WireGuardDbusRepoMock::init_conn_list_with_multi_active();
+        let id_to_activate = "some-id-2";
+        // Act
+        let result = ActivateConnectionUsecase::new(&dbus_repo_mock)
+            .activate(&WireGuardConnection::new(id_to_activate.into(), false));
+        // Assert
+        assert!(result.is_ok());
+        assert_eq!(dbus_repo_mock.get_active_connections().iter().len(), 1);
+        assert!(
+            dbus_repo_mock
+                .get_active_connections()
+                .first()
+                .expect("a single active connection exists here")
+                .get_is_active(),
+        );
+        assert_eq!(
+            dbus_repo_mock
+                .get_active_connections()
+                .first()
+                .expect("a single active connection exists here")
+                .get_id(),
+            id_to_activate
+        );
+    }
+}
