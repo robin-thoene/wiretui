ed05f36142ac16e8aa6d30a1558270306d63de52
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2026-05-08 21:43:41 +0200

    wip(import): add keybind to import conn to help popup

--- crates/adapters/src/inbound/tui/custom_widgets/keymaps_popup.rs
+++ crates/adapters/src/inbound/tui/custom_widgets/keymaps_popup.rs
@@ -20,6 +20,7 @@ impl Widget for KeymapsPopup {
             Line::from("  j          Scroll up"),
             Line::from("  k          Scroll up"),
             Line::from("SPACE        Toggle connection"),
+            Line::from("  i          Import a new connection"),
         ];
         let help_popup_content = Paragraph::new(keymaps).block(
             Block::bordered()
@@ -38,8 +39,7 @@ impl Widget for KeymapsPopup {
 ///
 /// * `area` - The area that will partially be covered by the popup
 fn popup_area(area: Rect) -> Rect {
-    // TODO: find a better way that scales automatically in height and with
-    let vertical = Layout::vertical([Constraint::Length(7)]).flex(Flex::Center);
+    let vertical = Layout::vertical([Constraint::Length(8)]).flex(Flex::Center);
     let horizontal = Layout::horizontal([Constraint::Length(60)]).flex(Flex::Center);
     let [area] = vertical.areas(area);
     let [area] = horizontal.areas(area);
